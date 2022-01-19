aa13cf07b6021622fcf671c5381497139d733b79
Hitesh Gahlot <its.hitzzera.2020@gmail.com>
2022-01-19 19:36:24 +0530

    Create Java Variables.java

---
+++ Java Variables.java
@@ -0,0 +1,12 @@
+class Main {
+public static void main(String[] args) {
+int a = 1;                         //Integer
+float b = 2.2;                    //Float
+double c = 3.1444;               //Double
+char d = c;                     //Character
+String str = "Hitesh";         //String
+int binary = 0b010100;        //Binary number start with 0b (0-1)
+int octal = 0445;          //Octal number start with 0 (0-7)
+int decimal = 789;        //Decimal number(0-9)
+int hexadecimal = 0x8CB  //Hexadecial start with 0x (0-9; 10-A, 11-B, 12-C, 13-D, 14-E, 15-F) [0-15]
+  
