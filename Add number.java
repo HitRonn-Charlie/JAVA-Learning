f9fa7684ce01f0b1ec642d15adbbf81e2f3193b9
Hitesh Gahlot <its.hitzzera.2020@gmail.com>
2022-01-19 20:41:12 +0530

    Create Add number.java

---
+++ Add number.java
@@ -0,0 +1,12 @@
+class Main{
+  public static void main(String[] args) {
+    System.out.println("Enter two number");
+    int num1 = 23;
+    int num2 = 25;
+    System.out.println(num1+" "+num2);
+    sum = num1 + num2 ;
+    System.out.println("The sum is"+sum);
+  }
+}
+ 
+    
