190f52c090cc22544dddb32911cf2665b100dcb7
Hitesh Gahlot <its.hitzzera.2020@gmail.com>
2022-01-19 21:44:35 +0530

    Update Add number.java

--- Add number.java
+++ Add number.java
@@ -4,8 +4,8 @@ class Main{
     int num1 = 23;
     int num2 = 25;
     System.out.println(num1+" "+num2);
-    sum = num1 + num2 ;
-    System.out.println("The sum is"+sum);
+    int sum = num1 + num2 ;
+    System.out.println("The sum is"+ " "+sum);
   }
 }
  
