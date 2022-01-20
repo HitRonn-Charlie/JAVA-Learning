736d337c32ef88bc344b14230fd73c413ce544dd
Hitesh <its.hitzzera.2020@gmail.com>
2022-01-20 09:11:45 +0530

    Floating multiplication

---
+++ Multiplyfloating.java
@@ -0,0 +1,11 @@
+public class Multiplyfloating {
+    public static void main(String[] args){
+
+        float num1 = 2.2f;
+        float num2 = 2.2f;
+
+        float product = num1 * num2 ;
+
+        System.out.println("The product is: " + product);
+    }
+}
