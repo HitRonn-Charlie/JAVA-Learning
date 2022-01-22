c426adb5a8610c0cfe01f86963f5f7c3c8902362
Hitesh <its.hitzzera.2020@gmail.com>
2022-01-22 19:18:27 +0530

    Evenodd

---
+++ Evenodd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Evenodd {
+
+    public static void main(String[] args) {
+
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int num = reader.nextInt();
+
+            if(num%2 == 0)
+            System.out.println(num + " is even");
+            else
+            System.out.println(num + " is odd");
+        }
+
+    }
+}
