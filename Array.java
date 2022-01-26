7d24e4f25eaf6aa1a2698b37c340b8e68107708b
Hitesh <its.hitzzera.2020@gmail.com>
2022-01-26 20:36:50 +0530

    Array initialization and accesss array element

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array{
+
+    public static void main(String[] args){
+
+int[] age = {2,4,6,8,9};
+
+System.out.println("First element: " + age[0]);
+
+System.out.println("Third element: " + age[2]);
+
+
+    }
+}
