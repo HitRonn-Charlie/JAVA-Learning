96e4e0f539d4fdfa384a33b2eff5eac633307362
Hitesh <its.hitzzera.2020@gmail.com>
2022-01-28 09:09:58 +0530

    String in java

---
+++ String.java
@@ -0,0 +1,50 @@
+class Strings {
+
+    public static void main(String[] args) {
+        
+    
+    //Create String
+
+     String first = "Java ";
+     String second = "Programming";
+
+
+   
+   
+   // String length
+
+   int Length = first.length();
+
+   System.out.println("Length of String: " + Length);
+
+
+
+
+
+   //Join string two method
+
+   System.out.println(first + second);
+
+   String joinString = first.concat(second);
+   
+   System.out.println("Joined string: " + joinString);
+
+
+   
+
+   //Compare String two method
+
+   boolean Compare = first.equals(second);
+
+   System.out.println("String first and second are equals: " + Compare);
+
+  
+   
+   
+   
+    }
+
+
+
+
+}
