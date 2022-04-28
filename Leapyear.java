80b96ee9516aa6d7b5c9fea5cf2baa9e54149bc7
HitRonn-Charlie <its.hitzzera.2020@gmail.com>
2022-04-28 08:23:54 +0530

    Add comments for understanding

--- Leapyear.java
+++ Leapyear.java
@@ -4,45 +4,58 @@ public class Leapyear{
 
     public static void main(String[] args) {
         
+
+        //Default value of boolean is false
         boolean leap = false;
 
-        //Input a year from user
 
+        //Input a year from user
         System.out.println("Enter a year: ");
 
         
-
+        //Input
         Scanner user = new Scanner(System.in);
-
         int year = user.nextInt();
 
+
+        //Condition to check: if year % 4==0 bcz leap year comes in every 4 year 
         if(year % 4 == 0) {
 
+
+            //Check given input is century year 
             if(year % 100 == 0) {
 
+
+                /*if century year then for a leap yaer input should be completely divisible by 
+                400*/
                 if(year % 400 == 0) {
 
                     leap = true;
 
                 }
 
+                //Not divisional by 400 then condition false and not a leap year
                 else {
                     leap = false;
                 }
-
+   
             }
 
+            //if input not a century year then it is a leap year
             else {
                 leap = true;
             }
 
 
+            
         }
 
+        //if input not completely divisible by 4 then not a leap year
         else {
             leap = false;
         }
 
+        
         user.close();
 
         if(leap ) {
